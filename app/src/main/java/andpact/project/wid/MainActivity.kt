70ac9d577a40e1692c77378ebaec45b3ef4a0d52
andpact <andpact@gmail.com>
2023-08-25 11:07:19 +0900

    add navigation bar

--- app/src/main/java/andpact/project/wid/MainActivity.kt
+++ app/src/main/java/andpact/project/wid/MainActivity.kt
@@ -3,41 +3,146 @@ package andpact.project.wid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import andpact.project.wid.ui.theme.WiDTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WiDTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    default()
-                }
-            }
+            WiDMainScreen()
         }
     }
 }
 
 @Composable
-fun default() { // 예를 수정하면 될듯.
-    Text(text = "Hello !")
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = Destinations.WiDCreateScreen.route) {
+        composable(Destinations.WiDCreateScreen.route) {
+            WiDCreateScreen()
+        }
+        composable(Destinations.WiDReadScreen.route) {
+            WiDReadScreen()
+        }
+        composable(Destinations.WiDSearchScreen.route) {
+            WiDSearchScreen()
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    navController: NavHostController, state: MutableState<Boolean>, modifier: Modifier = Modifier
+) {
+    val screens = listOf(
+        Destinations.WiDCreateScreen, Destinations.WiDReadScreen, Destinations.WiDSearchScreen
+    )
+
+    NavigationBar(
+        modifier = modifier,
+        containerColor = Color.LightGray,
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        screens.forEach { screen ->
+
+            NavigationBarItem(
+                label = {
+                    Text(text = screen.title!!)
+                },
+                icon = {
+                    Icon(imageVector = screen.icon!!, contentDescription = "")
+                },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo(navController.graph.findStartDestination().id) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    unselectedTextColor = Color.Gray, selectedTextColor = Color.White
+                ),
+            )
+        }
+    }
+
+}
+
+@Composable
+fun WiDMainScreen() {
+    WiDTheme() {
+        val navController: NavHostController = rememberNavController()
+//        val bottomBarHeight = 56.dp
+//        val bottomBarOffsetHeightPx = remember { mutableStateOf(0f) }
+
+        val buttonsVisible = remember { mutableStateOf(true) }
+
+        Scaffold(
+            bottomBar = {
+                BottomBar(
+                    navController = navController,
+                    state = buttonsVisible,
+                    modifier = Modifier
+                )
+            }) { paddingValues ->
+            Box(
+                modifier = Modifier.padding(paddingValues)
+            ) {
+                NavigationGraph(navController = navController)
+            }
+        }
+    }
+}
+
+sealed class Destinations(
+    val route: String,
+    val title: String? = null,
+    val icon: ImageVector? = null
+) {
+    object WiDCreateScreen : Destinations(
+        route = "wid_create_screen",
+        title = "Create",
+        icon = Icons.Filled.Add
+    )
+
+    object WiDReadScreen : Destinations(
+        route = "wid_read_screen",
+        title = "Read",
+        icon = Icons.Filled.List
+    )
+
+    object WiDSearchScreen : Destinations(
+        route = "wid_search_screen",
+        title = "Search",
+        icon = Icons.Filled.Search
+    )
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    WiDTheme {
-        default()
-    }
+fun WiDMainScreenPreview() {
+    WiDMainScreen()
 }
---
+++ app/src/main/java/andpact/project/wid/WiDSearchScreen.kt
@@ -0,0 +1,38 @@
+package andpact.project.wid
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WiDSearchScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Blue)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Search Screen",
+            style = MaterialTheme.typography.titleLarge,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WiDSearchScreenPreview() {
+    WiDSearchScreen()
+}
